3f8b33122855ccc12f82a52cb15272f79a0f9b9d
KBL <kibinlee19k@gmail.com>
2020-04-15 14:16:18 +0900

    Note Route

--- src/Routes/Note/Note.js
+++ src/Routes/Note/Note.js
@@ -3,87 +3,45 @@ import { Query } from "react-apollo";
 import { Link } from "react-router-dom";
 // import { ReactComponent as Plus } from "../../Components/plus.svg";
 import styled from "styled-components";
-import { GET_NOTES } from "../../queries";
+import MarkdownRenderer from "react-markdown-renderer";
+import { GET_NOTE } from "../../queries";
 
-const Header = styled.div`
+const TitleComponent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   margin-bottom: 50px;
 `;
 
 const Title = styled.h1`
   font-size: 50px;
-  font-weight: 600;
   margin: 0;
-  margin-bottom: 30px;
-  display: flex;
-  align-items: center;
+  padding: 0;
 `;
-
-const Button = styled.div`
-  margin-left: 10px;
-  transform: scale(0.8);
-  background-color: #eee;
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  border-radius: 10px;
-  cursor: pointer;
-`;
-
-const Subtitle = styled.h2`
-  color: #a2a19e;
-  font-weight: 400;
-`;
-
-const Notes = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-const Note = styled.div`
-  padding: 10px;
-  padding-left: 5px;
-  transition: background-color 0.1s linear;
-  cursor: pointer;
-  border-radius: 5px;
-  margin-bottom: 10px;
-  &:hover {
-    background-color: #eeeeee;
-  }
-`;
-
-const NoteTitle = styled.span`
-  padding-bottom: 5px;
-  font-weight: 600;
-  font-size: 20px;
-`;
+const Button = styled.button``;
 
 function Notte() {
+  const {
+    match: {
+      params: { id },
+    },
+  } = this.props;
   return (
-    <>
-      <Header>
-        <Title>
-          Cache Notes
-          <Link to={"/add"}>
-            <Button>
-              <Title text={"btn"} />
-            </Button>
-          </Link>
-        </Title>
-        <Subtitle>Taking notes while we learn.</Subtitle>
-      </Header>
-      <Query query={GET_NOTES}>
-        {({ data }) =>
-          data.notes
-            ? data.notes.map((note) => (
-                <Link to={`/edit/${note.id}`} key={note.id}>
-                  <Note>{note.title}</Note>
-                </Link>
-              ))
-            : null
-        }
-      </Query>
-    </>
+    <Query query={GET_NOTE} variables={{ id }}>
+      {({ data }) =>
+        data.note ? (
+          <>
+            <TitleComponent>
+              <Title>{data.note && data.note.title}</Title>
+              <Link to={`/edit/${data.note.id}`}>
+                <Button>Edit</Button>
+              </Link>
+            </TitleComponent>
+            <MarkdownRenderer markdown={data.note.content} />
+          </>
+        ) : null
+      }
+    </Query>
   );
 }
 
